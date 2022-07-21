419975d6064a5f189239354594100d720a96d784
shimi bukshpan <buki8060@gmail.com>
2022-07-21 08:57:17 +0300

    change file app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const port = 5000;
 //mongoose = mongo db
 var mongoose = require("mongoose");
-mongoose.Promise = global.Promise;mongoose.connect("mongodb://localhost:27017/homeWorkNodeJS");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/homeWorkNodeJS");
 let mySchema = new mongoose.Schema({
  firstname: String,
  lastname: String,
@@ -10,7 +11,6 @@ let mySchema = new mongoose.Schema({
  phon:Number
 });
 let User = mongoose.model("User", mySchema);
-
 
 let express = require('express');
 let app = express();
@@ -26,6 +26,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('', (req, res) => {
+    let myData = new User(req.query);
+    myData.save()
     console.log(req.query);
     res.render('index');
 });
@@ -33,12 +35,12 @@ app.get('', (req, res) => {
 app.post('', (req, res) => {
     let myData = new User(req.body);
     myData.save()
-    .then(item =>{
-        res.send("item saved to database");
-    })
-    .catch(err =>{
-        res.status(400).send("unable to save to database");
-    });
+    // .then(item =>{
+    //     res.send("item saved to database");
+    // })
+    // .catch(err =>{
+    //     res.status(400).send("unable to save to database");
+    // });
     console.log(req.body);
     res.render('savedform');
 });
